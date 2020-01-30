e57b6f5f0f539ef7b6ced48650ca2d1dd9547d71
Yan <henryqi91@qq.com>
2020-01-30 17:00:46 +0800

    updated: U3L2-Python本地文档与二进制数据存储

--- 第三单元/第二课-python本地文档与二进制数据存储/python本地文档与二进制数据存储.py
+++ 第三单元/第二课-python本地文档与二进制数据存储/python本地文档与二进制数据存储.py
@@ -1,11 +1,30 @@
+"""小块读取"""
+import requests, uuid
+
+def get_img(url):
+    img = requests.get(url=url,stream=True)
+    img_name = "{}.jpg".format(uuid.uuid4())
+    with open(img_name,'wb') as f:
+        chunks = img.iter_content(chunk_size=128)
+        for chunk in chunks:
+            f.write(chunk)
+
+if __name__ == '__main__':
+    url = "https://www.baidu.com"
+    get_img(url)
+
+
 """Python操作文本"""
 # r - 读取文件
 # w - 创建文件（覆盖原文件）
 # a - 追加文件（不存在会创建）
 # b - 操作二进制流
 # + - rw的集合
-with open('text.txt', 'r')as f:
-    print(f.read())
+# with open('text.txt', 'r')as f:
+#     # print(f.read())
+#     content = f.readlines()
+#     for c in content:
+#         print(c.strip())
 
 """uuid"""
 # import uuid
