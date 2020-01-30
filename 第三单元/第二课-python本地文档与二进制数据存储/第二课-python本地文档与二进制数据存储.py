4dcf353ec1aa77d41efce8d4618b939572ccc619
Yan <henryqi91@qq.com>
2020-01-30 16:56:19 +0800

    updated: U3L2-Python本地文档与二进制数据存储

--- 第三单元/第二课-python本地文档与二进制数据存储/第二课-python本地文档与二进制数据存储.py
+++ 第三单元/第二课-python本地文档与二进制数据存储/第二课-python本地文档与二进制数据存储.py
@@ -1,11 +1,20 @@
+"""Python操作文本"""
+# r - 读取文件
+# w - 创建文件（覆盖原文件）
+# a - 追加文件（不存在会创建）
+# b - 操作二进制流
+# + - rw的集合
+with open('text.txt', 'r')as f:
+    print(f.read())
+
 """uuid"""
-import uuid
-print("uuid1:", uuid.uuid1())
-print("uuid4:", uuid.uuid4())
-
-from uuid import UUID
-print("uuid3:", uuid.uuid3(UUID(int=1),name="123"))
-
+# import uuid
+# print("uuid1:", uuid.uuid1())
+# print("uuid4:", uuid.uuid4())
+#
+# from uuid import UUID
+# print("uuid3:", uuid.uuid3(UUID(int=1),name="123"))
+#
 
 
 """OS文件目录"""
